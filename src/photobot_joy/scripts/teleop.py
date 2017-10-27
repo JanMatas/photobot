15be955ef2e26a9ad2dae90b55cbbb9ed812f6e0
Jan Matas <janci5243@gmail.com>
2017-10-27 17:22:42 +0100

    Initial commit.

---
+++ src/photobot_joy/scripts/teleop.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+def callback(data):
+        twist = Twist()
+        twist.linear.x = 4*data.axes[1]
+        twist.angular.z = 4*data.axes[0]
+        pub.publish(twist)
+
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('listener', anonymous=True)
+    global pub
+    pub = rospy.Publisher('RosAria/cmd_vel', Twist)
+    rospy.Subscriber('joy', Joy, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
