6e6c73a2751e00ad143ca53ae3397dd263c95ea5
human4 <pavol.olexa@gmail.com>
2017-12-06 23:27:22 +0000

    final 1

--- src/photobot_joy/scripts/ui.py
+++ src/photobot_joy/scripts/ui.py
@@ -13,7 +13,7 @@ class UiPublisher(object):
         self.image_sub = rospy.Subscriber("/event_trigger",String,self.callback)#/camera/rgb/image_color
         self.counter = 0
     def callback(self, msg):
-        if (if "taken" in msg.data):
+        if ("taken" in msg.data):
             filename='/home/human4/photobot/camera_image.jpg'
             img_data = open(filename, 'rb').read()
             jpg_as_text = base64.b64encode(img_data)
