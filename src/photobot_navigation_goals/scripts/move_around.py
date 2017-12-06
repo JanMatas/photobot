15f9ddd3740b6dfbda37e12773d2e2c1aa569f47
Nick Karandejs <nick.karandejs@gmail.com>
2017-12-06 22:57:22 +0000

    wip; add node to send navigation goals selected from map

--- src/photobot_navigation_goals/scripts/move_around.py
+++ src/photobot_navigation_goals/scripts/move_around.py
@@ -18,45 +18,72 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import actionlib
 from actionlib_msgs.msg import *
 
-class GoForwardAvoid():
-    def __init__(self):
-        rospy.init_node('nav_test', anonymous=False)
-
-        #what to do if shut down (e.g. ctrl + C or failure)
-        rospy.on_shutdown(self.shutdown)
-        #tell the action client that we want to spin a thread by default
-        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        rospy.loginfo("wait for the action server to come up")
-        #allow up to 5 seconds for the action server to come up
-        self.move_base.wait_for_server(rospy.Duration(5))
-
-        #we'll send a goal to the robot to move 3 meters forward
-        goal = MoveBaseGoal()
-        goal.target_pose.header.frame_id = 'base_link'
-        goal.target_pose.header.stamp = rospy.Time.now()
-        goal.target_pose.pose.position.x = 3.0 #3 meters
-        goal.target_pose.pose.orientation.w = 1.0 #go forward
-        #start moving
-        self.move_base.send_goal(goal)
-        #allow TurtleBot up to 60 seconds to complete task
-        success = self.move_base.wait_for_result(rospy.Duration(60)) 
-        if not success:
-                    self.move_base.cancel_goal()
-                    rospy.loginfo("The base failed to move forward 3 meters for some reason")
-        else:
-            # We made it!
-            state = self.move_base.get_state()
-            if state == GoalStatus.SUCCEEDED:
-                rospy.loginfo("Hooray, the base moved 3 meters forward")
+fname = 'clicked_points.txt'
 
 
+def load_goals():
+    def to_xy(s):
+        s_ = s.split(', ')
+        return (float(s_[0], float(s_[1])))
 
-    def shutdown(self):
-        rospy.loginfo("Stop")
+    with open(fname) as f:
+        points = f.readlines()
+        return [to_xy(s) for p in points]
+
+
+def make_xy_goal(point):
+    goal = MoveBaseGoal()
+    goal.target_pose.header.frame_id = 'base_link'
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.pose.position.x = point[0]
+    goal.target_pose.pose.position.y = point[1]
+    goal.target_pose.pose.orientation.w = 1.0 #go forward
+    return goal
+
+
+def start_node():
+    rospy.init_node('wander_around_node', anonymous=False)
+
+    #what to do if shut down (e.g. ctrl + C or failure)
+    rospy.on_shutdown(shutdown)
+    #tell the action client that we want to spin a thread by default
+    move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+    rospy.loginfo("wait for the action server to come up")
+    #allow up to 5 seconds for the action server to come up
+    move_base.wait_for_server(rospy.Duration(5))
+    return move_base
+
+
+def goto_goal(move_base, point):
+    goal = make_xy_goal(point)
+    #start moving
+    move_base.send_goal(goal)
+    #allow TurtleBot up to 60 seconds to complete task
+    success = move_base.wait_for_result(rospy.Duration(60)) 
+    if not success:
+        move_base.cancel_goal()
+        rospy.loginfo("The base failed to move forward 3 meters for some reason")
+    else:
+        # We made it!
+        state = move_base.get_state()
+        if state == GoalStatus.SUCCEEDED:
+            rospy.loginfo("Hooray, the base moved to %f %f", point[0], point[1])
+
+
+def shutdown(self):
+    rospy.loginfo("Stop")
+
+
+def move_around():
+    #  goals = [(3, 0)]
+    goals = load_goals()
+    move_base = start_node()
+    for point in goals:
+        goto_goal(move_base, point)
 
 
 if __name__ == '__main__':
     try:
-        GoForwardAvoid()
+        move_around()
     except rospy.ROSInterruptException:
         rospy.loginfo("Exception thrown")
