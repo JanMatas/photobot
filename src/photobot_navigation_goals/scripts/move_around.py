453a1a86312d7dd12bdc04db84611f132e493208
Nick Karandejs <nick.karandejs@gmail.com>
2017-12-07 21:24:58 +0000

    Use class MoveAround; break goals if face detected

--- src/photobot_navigation_goals/scripts/move_around.py
+++ src/photobot_navigation_goals/scripts/move_around.py
@@ -4,13 +4,16 @@ from time import sleep
 
 import rospy
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import Bool
 import actionlib
 from actionlib_msgs.msg import *
 
 fname = "clicked_points.txt"
 face_detected_topic = "/face_detected"
 
-face_detected = False
+
+def shutdown():
+    rospy.loginfo("Stop")
 
 
 def load_goals():
@@ -33,65 +36,65 @@ def make_xy_goal(point):
     return goal
 
 
-#  def face_detected_callback(data):
-def face_detected_callback(data, args):
-    global face_detected
-    move_base = args[0]
-    if data.data == "Face detected":
-        face_detected = True
-        move_base.cancel_goal()
-        rospy.loginfo("Cancelling current goal")
+class MoveAround:
+    # This method has a bit weird signature because it's a callback
+
+    def __init__(self):
+        self.face_detected = False
+
+    @staticmethod
+    def face_detected_callback(data, self):
+        if data.data == True:
+            self.face_detected = True
+            self.move_base.cancel_goal()
+            rospy.loginfo("Face detected: cancelling current goal")
 
 
-def start_node():
-    rospy.init_node('wander_around_node', anonymous=False)
+    def start_node(self):
+        rospy.init_node('wander_around_node', anonymous=False)
 
-    move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-    rospy.Subscriber(face_detected_topic, String,
-                     face_detected_callback, (move_base))
-    rospy.on_shutdown(shutdown)
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.Subscriber(face_detected_topic, Bool,
+                         self.face_detected_callback, self)
+        rospy.on_shutdown(shutdown)
 
-    # Tell the action client that we want to spin a thread by default
-    rospy.loginfo("wait for the action server to come up")
+        # Tell the action client that we want to spin a thread by default
+        rospy.loginfo("wait for the action server to come up")
+        self.move_base.wait_for_server(rospy.Duration(5))
 
-    move_base.wait_for_server(rospy.Duration(5))
-    return move_base
+    def goto_goal(self, point):
+        #  global face_detected
+        timeout = 60
+        dt = 3
+        elapsed = 0
+        goal = make_xy_goal(point)
+        self.move_base.send_goal(goal)
+        rospy.loginfo("Goal sent!")
+        # Allow robot up to `timeout` seconds to complete the task
+        success = False
+        while not self.face_detected and elapsed < timeout:
+            success = self.move_base.wait_for_result(rospy.Duration(dt))
+            elapsed += dt
 
-
-def goto_goal(move_base, point):
-    timeout = 60
-    goal = make_xy_goal(point)
-    move_base.send_goal(goal)
-    #  start_time = time()
-    # Allow robot up to `timeout` seconds to complete the task
-    success = move_base.wait_for_result(rospy.Duration(timeout))
-    #  while move_base.get_state() != GoalStatus.SUCCEEDED \
-    #   and time()-start_time < timeout:
-    #      if face_detected:
-            #  move_base.cancel_goal()
-            #  rospy.loginfo("Face detected, cancelling goal")
-    #  success = move_base.get_state() == GoalStatus.SUCCEEDED
-    if not success:
-        move_base.cancel_goal()
-        rospy.loginfo("The base failed to reach goal in %d seconds", timeout)
-    else:
-        state = move_base.get_state()
-        if state == GoalStatus.SUCCEEDED:
-            rospy.loginfo("Moved to %f %f", point[0], point[1])
-
-
-def shutdown(self):
-    rospy.loginfo("Stop")
+        rospy.loginfo("Stopped waiting for goal")
+        if not success and not self.face_detected:
+            self.move_base.cancel_goal()
+            rospy.loginfo("The base failed to reach goal in %d seconds", timeout)
+        else:
+            state = self.move_base.get_state()
+            if state == GoalStatus.SUCCEEDED:
+                rospy.loginfo("Moved to %f %f", point[0], point[1])
 
 
 def move_around():
-    global face_detected
-    #  goals = [(3, 0)]
     goals = load_goals()
-    move_base = start_node()
+    move = MoveAround()
+    move.start_node()
     for point in goals:
-        if not face_detected:
-            goto_goal(move_base, point)
+        if not move.face_detected:
+            move.goto_goal(point)
+        else:
+            rospy.loginfo("Ignoring goal")
 
 
 if __name__ == '__main__':
